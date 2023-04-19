e1f4dcff7f413a0172c88685c1b1ab326f780461
Jiaqi Gu <droidqw@gmail.com>
2023-04-19 09:11:13 +0800

    fix

--- KeepChatGPT.js
+++
@@ -1,5 +0,0 @@
-let ele = '<head>';
-let eleReplace = '<head><script type="text/javascript" async="async" src="https://github.com/xcanwin/KeepChatGPT/raw/main/KeepChatGPT.user.js"></script>'
-let body = $response.body
-    .replace(ele, eleReplace)
-$done({ body });
