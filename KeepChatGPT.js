c5190f9cb27e5055fa5224e0988eb4f27e750b04
Jiaqi Gu <droidqw@gmail.com>
2023-04-19 09:35:08 +0800

    fix

---
+++ KeepChatGPT.js
@@ -0,0 +1,8 @@
+let ele = '<head>';
+let eleReplace = '<head><script type="text/javascript">
+$httpClient.get({url:'https://raw.githubusercontent.com/xcanwin/KeepChatGPT/main/KeepChatGPT.user.js'}, (error, response, body) => {
+    eleReplace = eleReplace + body + '</script>'
+});
+let body = $response.body
+    .replace(ele, eleReplace)
+$done({ body });
